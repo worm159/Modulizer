6675a940d15a1b4b5f3d0529ebbe8ea76ccaf4e3
worm159 <august.wurm@sport-hartkirchen.at>
2016-12-12 17:07:12 +0100

    awur

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,7 +1,10 @@
 package Test;
 
 import Test.factory.ProcessModelFactory;
+import uflow.data.function.immutable.ProcessFunction;
 import uflow.data.model.immutable.ProcessModel;
+import uflow.data.model.immutable.ProcessStepModel;
+import uflow.data.model.immutable.ProcessUnitModel;
 
 /**
  * Created by augus on 12.12.2016.
@@ -11,6 +14,16 @@ public class Test {
     public static void main (String [ ] args) {
         ProcessModel test = ProcessModelFactory.createPurchaseProduct();
 
-        System.out.println(test);
+        test.getAuthorizedStartRoles();
+
+        for (ProcessUnitModel unit : test.getProcessUnitModels().getValues() ) {
+            for (ProcessStepModel step : unit.getProcessStepModels().getValues()) {
+                for (ProcessFunction func : step.getProcessFunctions()) {
+                    System.out.println(func);
+                }
+            }
+        }
+
+        //System.out.println(test);
     }
 }
