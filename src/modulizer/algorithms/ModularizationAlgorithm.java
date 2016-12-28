83d53b78851d92e5a5c77e0cc73df905649f7597
Brigitte <brigitte.andorfer@gmx.at>
2016-12-28 09:20:20 +0100

    Interface ModularizationAlgorithm

---
+++ src/modulizer/algorithms/ModularizationAlgorithm.java
@@ -0,0 +1,10 @@
+package modulizer.algorithms;
+
+import uflow.data.model.immutable.ProcessModel;
+
+/**
+ * Created by Brigitte on 28.12.2016.
+ */
+public interface ModularizationAlgorithm {
+    public ProcessModel startModularization(ProcessModel model);
+}
--- src/modulizer/ui/ModulizerUI.java
+++ src/modulizer/ui/ModulizerUI.java
@@ -1,14 +1,25 @@
 package modulizer.ui;
 
+import uflow.data.model.immutable.ProcessModel;
+
 /**
  * Created by Brigitte on 28.12.2016.
  */
 public class ModulizerUI {
     public static void main(String[] args) {
+        // create Window
+        // selection of modularization algorithm
+        // upload of process model
 
+        // start of modularization
+        // get the modularized process model
+        ProcessModel modularized = ;
+
+        // print the modularized process model
+        printProcessModel(modularized);
     }
 
-    public static void printProcessModel() {
+    public static void printProcessModel(ProcessModel model) {
 
     }
 }
