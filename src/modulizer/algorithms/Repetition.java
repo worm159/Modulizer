e2f0876fd1937db687f3f650a42a4d0a3ab27d05
Brigitte <brigitte.andorfer@gmx.at>
2016-12-28 09:05:52 +0100

    Class structure

---
+++ src/modulizer/algorithms/Repetition.java
@@ -0,0 +1,7 @@
+package modulizer.algorithms;
+
+/**
+ * Created by Brigitte on 28.12.2016.
+ */
+public class Repetition {
+}
---
+++ src/modulizer/ui/ModulizerUI.java
@@ -0,0 +1,14 @@
+package modulizer.ui;
+
+/**
+ * Created by Brigitte on 28.12.2016.
+ */
+public class ModulizerUI {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printProcessModel() {
+
+    }
+}
