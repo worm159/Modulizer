ce5c5277636ae5d1986a0572398f61cc9bf284fe
Brigitte <brigitte.andorfer@gmx.at>
2017-01-05 10:54:53 +0100

    SESE

--- src/modulizer/algorithms/Repetition.java
+++ src/modulizer/algorithms/Repetition.java
@@ -3,13 +3,14 @@ package modulizer.algorithms;
 import uflow.data.model.immutable.ProcessModel;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Brigitte on 28.12.2016.
  */
 public class Repetition extends ModularizationAlgorithm {
     @Override
-    public List<ProcessModel> startModularization(ProcessModel model) {
+    public Map<String,ProcessModel> startModularization(ProcessModel model) {
         return null;
     }
 }
