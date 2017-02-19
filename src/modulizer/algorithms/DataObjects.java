5b349323aa82fd21e2824052db2f62abf785a088
Brigitte <brigitte.andorfer@gmx.at>
2017-02-19 19:52:41 +0100

    strukturelle Änderungen

--- src/modulizer/algorithms/DataObjects.java
+++ src/modulizer/algorithms/DataObjects.java
@@ -45,40 +45,64 @@ public class DataObjects extends ModularizationAlgorithm {
         return result;
     }
 
+    /**
+     * method that finds all dataObjects that are provided, required or proceeded by this step
+     *
+     * @param step the step that should be searched for dataObjects
+     */
     private void handleStep(Step step) {
-        ArrayList<String> dataObjects = new ArrayList<>();
-        dataObjects.addAll(step.getProvided());
-        dataObjects.addAll(step.getRequired());
+        // get the corresponding ProcessStepModel and create a copy of it
         ProcessStepModel processStep = modelToSplit.getProcessUnitModels().get(step.getUnitId())
                 .getProcessStepModels().get(step.getId());
         ProcessStepModel copiedStep = copyStep(processStep);
+        // put all the dataObjects of this Step in the Provide or Require Functions in a new List
+        ArrayList<String> dataObjects = new ArrayList<>();
+        dataObjects.addAll(step.getProvided());
+        dataObjects.addAll(step.getRequired());
+        // loop over all the dataObjects
         for(String dataObject : dataObjects) {
+            // get the model for this dataObject
             getCurrentModel(dataObject);
+            // get the ProcessUnitModelModifier for this step
             ProcessUnitModelModifier unitModifier = getUnitModifier(step);
+            // add the copied ProcessStepModel to the current model
             unitModifier.setProcessStepModel(step.getId(),copiedStep);
         }
+        // loop over all the ProcessFunctions of this step
         for (ProcessFunction func : processStep.getProcessFunctions()) {
             if(func.getClass().getName().equals("uflow.data.function.immutable.ProceedFunction")) {
+                // if it is a ProceedFunction save the function, the target Unit and the next Step
                 ProceedFunction f = (ProceedFunction) func;
                 String targetProcessUnit = f.getTargetProcessUnit();
                 String next = f.getNext();
+                // loop over all the dataObjects in this ProceedFunction
                 for(String dataObject : f.getValues().getKeys()) {
+                    // get the model for this dataObject
                     getCurrentModel(dataObject);
+                    // get the ProcessUnitModelModifier for this step
                     ProcessUnitModelModifier unitModifier = getUnitModifier(step);
+                    // add the copied ProcessStepModel to the current model
                     unitModifier.setProcessStepModel(step.getId(),copiedStep);
                     ProcessStepModel nextStep,copiedNextStep;
                     if (targetProcessUnit == null || targetProcessUnit.isEmpty()) {
+                        // if the unit is the same for the next step
+                        // get the next step from the current unit
                         nextStep = modelToSplit.getProcessUnitModels().get(step.getUnitId()).getProcessStepModels().get(next);
                         if(nextStep != null) {
+                            // copy the step and add it to the current model
                             copiedNextStep = copyStep(nextStep);
                             unitModifier.setProcessStepModel(next,copiedNextStep);
                         }
                     } else {
+                        // if the unit is different for the next step
+                        // get the ProcessUnitModelModifier for the next step
                         ProcessUnitModelModifier nextUnitModifier = getUnitModifier(steps.get(next));
                         if(nextUnitModifier != null) {
+                            // get the next step from the target unit
                             nextStep = modelToSplit.getProcessUnitModels().get(targetProcessUnit)
                                     .getProcessStepModels().get(next);
                             if(nextStep != null) {
+                                // copy the step and add it to the current model
                                 copiedNextStep = copyStep(nextStep);
                                 nextUnitModifier.setProcessStepModel(next,copiedNextStep);
                             }
